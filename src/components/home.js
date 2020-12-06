6772fc43f513136d42fb50495a5bace72231e6eb
karina2004liz <eng.karina.cabrera@gmail.com>
2020-12-06 16:02:46 -0600

    Se instala librería para efecto Swipe en carga de info

--- src/components/home.js
+++ src/components/home.js
@@ -4,10 +4,28 @@ import LogoVenados from "./../assets/Venadosfc.png";
 import "antd/dist/antd.css";
 import { Tabs } from "antd";
 import "./home.css";
+import { PullToRefresh } from "react-js-pull-to-refresh";
+import {
+  PullDownContent,
+  ReleaseContent,
+  RefreshContent,
+} from "react-js-pull-to-refresh";
+
 const { TabPane } = Tabs;
 
 const Home = () => {
   const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const onRefreshData = () => {
+    return VenadosServices.getGames()
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        Promise.reject();
+      });
+  };
 
   useEffect(() => {
     VenadosServices.getGames().then((data) => {
@@ -29,7 +47,19 @@ const Home = () => {
         style={{ width: "100%" }}
       >
         <TabPane tab="COPA MX" key={1}>
-          COsa 1
+          <PullToRefresh
+            pullDownContent={<PullDownContent />}
+            refreshContent={<RefreshContent />}
+            pullDownThreshold={100}
+            onRefresh={onRefreshData}
+            triggerHeight={50}
+            backgroundColor="white"
+            startInvisible={true}
+          >
+            <div style={{ height: "40vh", textAlign: "center" }}>
+              <div>PullToRefresh</div>
+            </div>
+          </PullToRefresh>
         </TabPane>
         <TabPane tab="ASCENSO MX" key={2}>
           Cosa 2
