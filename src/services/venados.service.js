4e990bca2ea5407187e0a2408442fb3741443a5c
karina2004liz <eng.karina.cabrera@gmail.com>
2020-12-09 13:17:51 -0600

    Limpiando peticiones en código de services

--- src/services/venados.service.js
+++ src/services/venados.service.js
@@ -1,79 +1,67 @@
 const API_URL = "https://venados.dacodes.mx/api";
 
-const getDataMovies = "https://api.themoviedb.org/3/movie/now_playing?api_key=634b49e294bd1ff87914e7b9d014daed&language=en-US"
+const getDataMovies =
+  "https://api.themoviedb.org/3/movie/now_playing?api_key=634b49e294bd1ff87914e7b9d014daed&language=en-US";
 
 const getGames = () => {
-
-return fetch(getDataMovies, { method: "GET"}).then(
-    handleResponse
-  );
+  return fetch(getDataMovies, { method: "GET" }).then(handleResponse);
 };
 
+//Estas son las funciones que se usarían para consumir la api que falló
 const getStadistics = () => {
+  return fetch(API_URL + "/statistics", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  }).then(handleResponse);
+};
 
-    return fetch(API_URL + "/statistics", { method: "GET", headers: {
-        "Accept": "application/json",
-      }}).then(
-        handleResponse
-      );
-    };
+const getPlayers = () => {
+  return fetch(API_URL + "/players", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  }).then(handleResponse);
+};
 
+const getSponsors = () => {
+  return fetch(API_URL + "/sponsors", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  }).then(handleResponse);
+};
 
-    const getPlayers = () => {
-
-        return fetch(API_URL + "/players", { method: "GET", headers: {
-            "Accept": "application/json",
-          }}).then(
-            handleResponse
-          );
-        };
-
-        const getSponsors = () => {
-
-            return fetch(API_URL + "/sponsors", { method: "GET", headers: {
-                "Accept": "application/json",
-              }}).then(
-                handleResponse
-              );
-            };
-
-            const getNotifications = () => {
-
-                return fetch(API_URL + "/notifications", { method: "GET", headers: {
-                    "Accept": "application/json",
-                  }}).then(
-                    handleResponse
-                  );
-                };
-
-
+const getNotifications = () => {
+  return fetch(API_URL + "/notifications", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  }).then(handleResponse);
+};
 
 function handleResponse(response) {
   return response.text().then((text) => {
     const data = text && JSON.parse(text);
     if (response.status !== 200) {
-      if (response.status === 401 || response.status === 500) {
-        // auto logout if 401 response returned from api
-        window.location.reload("/");
-        //!Activar la redireccion!!
-      }
-
       const error = (data && data.message) || response.statusText;
       console.log(error);
       return Promise.reject(error);
     }
-
-      return data;
-    
+    return data;
   });
 }
 
- const VenadosService = {
+const VenadosService = {
   getGames,
   getNotifications,
   getPlayers,
   getStadistics,
-  getSponsors
+  getSponsors,
 };
 
-export default VenadosService
+export default VenadosService;
